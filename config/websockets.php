4c753884622fd191d275574828c07eb6ecffb760
guilherme miranda martins martins <guiframart1@gmail.com>
2024-09-09 03:53:36 -0700

    reconfigurarwebsocket

--- config/websockets.php
+++ config/websockets.php
@@ -116,13 +116,13 @@ return [
          * certificate chain of issuers. The private key also may be contained
          * in a separate file specified by local_pk.
          */
-        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', 'C:\xampp\apache\conf\ssl.crt\laravel.crt'),
+        'local_cert' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_CERT', 'C:\xampp\apache\conf\ssl.crt\startupinvest.crt'),
 
         /*
          * Path to local private key file on filesystem in case of separate files for
          * certificate (local_cert) and private key.
          */
-        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', 'C:\xampp\apache\conf\ssl.key\laravel.key'),
+        'local_pk' => env('LARAVEL_WEBSOCKETS_SSL_LOCAL_PK', 'C:\xampp\apache\conf\ssl.key\startupinvest.key'),
 
         /*
          * Passphrase for your local_cert file.
